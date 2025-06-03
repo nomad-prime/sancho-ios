5a395cad0c3ae0b12bf264fe98e3da2399ac158b
Armin Jazi <a.ghajarjazi@reply.de>
2025-06-03 08:01:52 +0200

    Initial Commit

---
+++ sanchoTests/sanchoTests.swift
@@ -0,0 +1,17 @@
+//
+//  sanchoTests.swift
+//  sanchoTests
+//
+//  Created by Armin Ghajar Jazi on 03.06.25.
+//
+
+import Testing
+@testable import sancho
+
+struct sanchoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
