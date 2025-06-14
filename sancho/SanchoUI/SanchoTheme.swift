6a0d49a9e86fd59f234c958881e404f857387406
Armin Jazi <a.ghajarjazi@reply.de>
2025-06-14 10:47:09 +0200

    update themes and colorS

--- sancho/SanchoUI/SanchoTheme.swift
+++ sancho/SanchoUI/SanchoTheme.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
 struct SanchoTheme {
-    static let primaryColor = Color("PrimaryColor")
+    static let primaryColor = Color("SanchoPrimaryColor")
     static let accentColor = Color("AccentColor")
+    static let backgroundTint = Color("SanchoBackgroundColor")
     static let cornerRadius: CGFloat = 8.0
     static let spacing: CGFloat = 8.0
 }
-
